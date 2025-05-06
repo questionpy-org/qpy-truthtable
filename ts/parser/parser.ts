ca91f9ea04a88753c7d849e69e9566ae2be0216e
Jan Britz <britz@tu-berlin.de>
2025-05-06 11:10:25 +0200

    feat: only view brackets when explicitly set

--- ts/parser/parser.ts
+++ ts/parser/parser.ts
@@ -6,7 +6,7 @@ import {
 
 import BoraLexer from "./generated/BoraLexer.js";
 import BoraParser, {
-    AndOperatorContext,
+    AndOperatorContext, ExprContext,
     GroupFactorContext,
     ImplicitAndOperatorContext,
     ImpliesOperatorContext,
@@ -23,6 +23,10 @@ import BoraVisitor from "./generated/BoraVisitor.js";
 
 export type OutputFormat = "MATHEMATICAL" | "SYMBOLICAL";
 
+interface BinaryOperatorContext {
+    expr(i: number): ExprContext,
+}
+
 class BoraVisitorImplementation extends BoraVisitor<string> {
     private readonly format: OutputFormat;
 
@@ -31,59 +35,46 @@ class BoraVisitorImplementation extends BoraVisitor<string> {
         this.format = format;
     }
 
-    visitAndOperator = (ctx: AndOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
+    #getBinaryOperands = (ctx: BinaryOperatorContext) => [this.visit(ctx.expr(0)), this.visit(ctx.expr(1))];
 
+    visitAndOperator = (ctx: AndOperatorContext) => {
+        const [left, right] = this.#getBinaryOperands(ctx);
         return this.format === "MATHEMATICAL" ? `${left} \\cdot ${right}` : `${left} \\land ${right}`;
     };
 
     visitOrOperator = (ctx: OrOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return this.format === "MATHEMATICAL" ? `(${left} + ${right})` : `(${left} \\lor ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return this.format === "MATHEMATICAL" ? `${left} + ${right}` : `${left} \\lor ${right}`;
     };
 
     visitNotOperator = (ctx: NotOperatorContext) => {
         const factor = this.visit(ctx.factor());
-
-        return this.format === "MATHEMATICAL" ? `\\overline{${factor}}` : `\\neg (${factor})`;
-    };
+        return this.format === "MATHEMATICAL" ? `\\overline{${factor}}` : `\\neg ${factor}`;
+    }
 
     visitNorOperator = (ctx: NorOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return `(${left} ⊽ ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return `${left} ⊽ ${right}`;
     };
 
     visitNandOperator = (ctx: NandOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return `(${left} ⊼ ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return `${left} ⊼ ${right}`;
     };
 
     visitXorOperator = (ctx: XorOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return `(${left} ⊻ ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return `${left} ⊻ ${right}`;
     };
 
     visitXnorOperator = (ctx: XnorOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return `(${left} \\iff ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return `${left} \\iff ${right}`;
     };
 
     visitImpliesOperator = (ctx: ImpliesOperatorContext) => {
-        const left = this.visit(ctx.expr(0));
-        const right = this.visit(ctx.expr(1));
-
-        return `(${left} \\implies ${right})`;
+        const [left, right] = this.#getBinaryOperands(ctx);
+        return `${left} \\implies ${right}`;
     };
 
     visitImplicitAndOperator = (ctx: ImplicitAndOperatorContext) => {
@@ -95,7 +86,7 @@ class BoraVisitorImplementation extends BoraVisitor<string> {
 
     visitVariable = (ctx: VariableContext) => ctx.getText();
 
-    visitGroupFactor = (ctx: GroupFactorContext) => this.visit(ctx.expr());
+    visitGroupFactor = (ctx: GroupFactorContext) => `(${this.visit(ctx.expr())})`;
 
     visitLiteralTrue = () => (this.format === "MATHEMATICAL" ? "1" : "\\top");
 
