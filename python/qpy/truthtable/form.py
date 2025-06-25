81d0b9e66d603705b58279d08f9571da214ae2cd
Jan Britz <britz@tu-berlin.de>
2025-06-25 14:57:30 +0200

    feat: validate formula

--- python/qpy/truthtable/form.py
+++ python/qpy/truthtable/form.py
@@ -1,3 +1,6 @@
+from pydantic import field_validator
+from pydantic_core.core_schema import ValidationInfo
+
 from questionpy.form import (
     FormModel,
     text_input,
@@ -29,3 +32,17 @@ class MyModel(FormModel):
     generate_formula = checkbox("Formel generieren")
     custom_formula = group("", CustomFormula, hide_if=is_checked("generate_formula"))
     generated_formula_options = group("Formel Optionen", GeneratedFormulaOptions, hide_if=is_not_checked("generate_formula"))
+
+    @field_validator("custom_formula", mode="after")
+    @classmethod
+    def validate_custom_formula(cls, value: CustomFormula, values: ValidationInfo):
+        if not values.data.get("generate_formula", True) and not value.formula:
+            raise ValueError("Bitte geben Sie eine Formel an.")
+        return value
+
+    @field_validator("generate_formula", mode="after")
+    @classmethod
+    def validate_generate_formula(cls, value: bool, _values: ValidationInfo):
+        if value:
+            raise ValueError("Formeln können derzeit nicht generiert werden.")
+        return value
